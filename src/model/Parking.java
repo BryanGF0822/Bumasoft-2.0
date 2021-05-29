323c20667317d04544eb36eedd382155c5b8521f
BryanGF0822 <bryanicesi0822@gmail.com>
2021-05-28 23:32:23 -0500

    Update of Parking.

--- src/model/Parking.java
+++ src/model/Parking.java
@@ -2,7 +2,19 @@ package model;
 
 public class Parking {
 	
-	Parking(){
-		
+	private String name;
+
+	public Parking(String name) {
+		this.name = name;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
